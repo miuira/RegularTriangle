250f1db0e030bb7bc4843a752efd9ee4860ad073
Irina <Irina@IRINA-PC>
2017-07-19 09:10:03 +0300

    new

---
+++ RegularTriangle/Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularTriangle
+{
+    public class Triangle
+    {
+        // длины сторон
+        int a, b, c;
+
+        // конструктор
+        public Triangle()
+        {
+            this.a = 1;
+            this.b = 1;
+            this.c = 1;
+        }
+
+        // конструктор с параметрами
+        public Triangle(int a, int b, int c)
+        {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+        }
+
+        // расчет площади прямоугольного треугольника
+        public double Area()
+        {
+            double area=0;
+            try
+            {
+                // проверка на значение стороны
+                if (a <= 0 || b <= 0 || c <= 0)
+                    throw new ArgumentException("Значение длины стороны должно быть положительным числом!");
+                area = 0.5 * a * b;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return area;
+        }
+    }
+}
---
+++ TriangleTests/TriangleTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegularTriangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularTriangle.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+
+        [TestMethod()]
+        public void ValidArea()
+        {
+            Triangle t = new Triangle(1, 2, 3);
+            double exp = 1;
+            double act = t.Area();
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void NullArea()
+        {
+            Triangle t = new Triangle(0, 2, 3);
+            double exp = 0;
+            double act = t.Area();
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void MinusArea()
+        {
+            Triangle t = new Triangle(-1, 2, 3);
+            double exp = 0;
+            double act = t.Area();
+            Assert.AreEqual(exp, act);
+        }
+    }
+}
